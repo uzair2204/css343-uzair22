d27a80b66a68ffbc04133140a79dd602d894d797
uzair2204 <107969590+uzair2204@users.noreply.github.com>
2022-07-12 20:38:08 -0700

    Update 5a.cpp

--- HW2/5a.cpp
+++ HW2/5a.cpp
@@ -1,1 +1,38 @@
+/*
+Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
 
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        connect_helper(root);
+        return root;
+    }
+
+    void connect_helper(Node* root) {
+        if (root && root->left) {
+            if (root->next != nullptr) {
+                root->right->next = root->next->left;
+            }
+            root->left->next = root->right;
+            connect_helper(root->left);
+            connect_helper(root->right);
+        }
+    }
+
+};
