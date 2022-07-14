e60c240ea5bbd5d2b7259ed50505d1d31bc4ff77
Uzair Ahmad <107969590+uzair2204@users.noreply.github.com>
2022-07-14 00:18:17 -0700

    Update 5d.cpp

--- HW2/5d.cpp
+++ HW2/5d.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    //Function that takes in a TreeNode* and returns true if the tree is valid.
     bool isValidBST(TreeNode* root) {
         bool isValid = true;
         TreeNode* prev = nullptr;
@@ -19,15 +20,21 @@ public:
 
     }
     void inorder(TreeNode* root, TreeNode* &prev, bool& isValid) {
+        //Checking to see if the root node is null or not, then it checks to see if the boolean variable "isValid" is set to false
         if (root == nullptr || !isValid)
             return;
+        //Iterate through each of the nodes in order from left to right until it reaches a node with an equal 
+        //value on its left side and a greater value on its right side 
         inorder(root->left, prev, isValid);
+        //Attempts to check if a tree node is valid.
         if (prev && prev->val >= root->val)
         {
             isValid = false;
             return;
         }
+        //Declares a TreeNode* pointer named "prev" and initializes it with nullptr.
         prev = root;
+        //Iterates over the inorder function
         inorder(root->right, prev, isValid);
     }
 };
