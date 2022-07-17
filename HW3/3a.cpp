2567553a1d145d11858d15633df44fbfda99b892
Uzair Ahmad <107969590+uzair2204@users.noreply.github.com>
2022-07-17 14:19:14 -0700

    Update 3a.cpp

--- HW3/3a.cpp
+++ HW3/3a.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        Node* nodes[100];
+        for (int i = 0; i < 100; i++) {
+            nodes[i] = nullptr;
+        }
+        Node* copy = cloneGraph_helper(nodes, node);
+        return copy;
+    }
 
+    Node* cloneGraph_helper(Node* nodes[100], Node* node) {
+        if (!node)
+            return node;
+        if (nodes[node->val - 1])
+            return nodes[node->val - 1];
+        nodes[node->val - 1] = new Node();
+        nodes[node->val - 1]->val = node->val;
+        nodes[node->val - 1]->neighbors = vector<Node*>(node->neighbors.size(), nullptr);
+        for (int i = 0; i < node->neighbors.size(); i++) {
+            Node* copy = cloneGraph_helper(nodes, node->neighbors[i]);
+            nodes[node->val - 1]->neighbors[i] = copy;
+        }
+        return nodes[node->val - 1];
+    }
+};
