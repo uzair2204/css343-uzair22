a70feca798359298fd4ae560e92013e5477e1403
Uzair Ahmad <107969590+uzair2204@users.noreply.github.com>
2022-07-21 19:15:05 -0700

    Update 3f.cpp

--- HW3/3f.cpp
+++ HW3/3f.cpp
@@ -1,12 +1,15 @@
 class MedianFinder {
+    //Min Heap
     priority_queue<int, vector<int>, greater<int>> minHeap;
+    //Max Heap
     priority_queue<int, vector<int>, less<int>> maxHeap;
     
 public:
     MedianFinder() {
         
     }
-
+    
+    //Adds an integer to one of the queues based on its priority value.
     void addNum(int num) {
         maxHeap.push(num);
         num = maxHeap.top();
@@ -19,7 +22,8 @@ public:
             maxHeap.push(num);
         }
     }
-
+    
+    //Returns either the top item from maxHeap, minHeap or (minHeap + maxHeap) / 2.
     double findMedian() {
         return maxHeap.size() > minHeap.size() ? maxHeap.top() : double(minHeap.top() + maxHeap.top()) / 2;
     }
